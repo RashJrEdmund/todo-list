5a3d7ce4412e61d4c8e87350ef2df03fc3a0d1b3
rash <orashusedmund@gmail.com>
2023-01-12 15:18:57 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,82 +1,33 @@
 import './styles/styles.css';
-import updateTask from './scripts/data';
+import data from './scripts/data';
 import './images/tick.svg';
-
-// import {updateTask} from './scripts/data'
 
 const form = document.querySelector('.form');
 const inputField = document.querySelector('.input');
-
-const displayDiv = document.querySelector('.tasks');
-
-/* const addBtn = document.querySelector('.add-btn');
-const tickBtn = document.querySelector('.tick-btn'); */
-// const delBtn = document.querySelectorAll('.del-btn');
-
-/* const updateTasks = (task) => {
-  let template = '';
-  template += updateTask(task);
-  return template;
-}; */
-
-/* const addTasks = (task) => {
-
-}; */
+const resetBtn = document.querySelector('.reset-btn');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  // console.log(e.target);
   if (inputField.value === '' || inputField.value === ' ') {
     inputField.style = 'border: 2px solid red;';
   } else {
     inputField.style = 'border: unset;';
-    displayDiv.appendChild(updateTask(e.target.todoInput.value));
+    data.displayDiv.appendChild(data.updateTask(e.target.todoInput.value));
     e.target.todoInput.value = '';
-
-    const todosDivs = document.querySelectorAll('.todos'); // dragging and droping
-
-    todosDivs.forEach((draggable) => {
-      draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging');
-      });
-
-      draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('dragging');
-      });
-    });
   }
-  const delBtn = document.querySelectorAll('.del-btn');
-  delBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      console.log('Lord');
-    });
-  });
+
+  data.addEventHearers();
 });
 
-/* const todosDivs = document.querySelectorAll('.todos');
-
-todosDivs.forEach((draggable) => {
-  draggable.addEventListener('dragstart', () => {
-    draggable.classList.add('dragging');
-  });
-
-  draggable.addEventListener('dragend', () => {
-    draggable.classList.remove('dragging');
-  });
-}); */
-
-displayDiv.addEventListener('dragover', (e) => {
+resetBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  // const afterElement = getDragPosition(displayDiv, e.clientY);
-  const draggable = document.querySelector('.dragging'); // since only only one element will
-  //  have this dragging class, see line 76
-  displayDiv.appendChild(draggable);
+  document.querySelectorAll('.todos')
+    .forEach((todo) => {
+      setTimeout(() => {
+        todo.remove();
+      }, 400);
+    });
+
+  document.querySelector('input')
+    .value = '';
 });
-
-/*  const getDragPosition = (container, y) => {
-  draggableElements = [...container.querySelectorAll('draggable:not(dragging)')];
-
-  draggableElements.reduce((closest, child) => {
-
-  });
-}; */
--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,12 +1,10 @@
-// import { container } from 'webpack';
+const displayDiv = document.querySelector('.tasks');
 
 let uniCounter = 0;
 
 const updateTask = (task) => {
   const virtualDiv = document.createElement('div');
   virtualDiv.addEventListener('click', (/* event */) => {
-    console.log('jeez mourisee');
-    console.log(virtualDiv);
     // console.log(form.todoInput);
     // if (event.target.matches('')){}
   });
@@ -15,15 +13,14 @@ const updateTask = (task) => {
   virtualDiv.classList.add('todos');
   virtualDiv.draggable = true;
   virtualDiv.innerHTML = `
-  <div class="topic">${task}</div>
+  <div class="topic" data-counter="${virtualDiv.dataset.index}">${task}</div>
   <div class="buttons" data-value="${virtualDiv.dataset.index}">
-    <button class="tick-btn" name="cross">&cross;</button>
-    <button class="del-btn" name="tick">&check;</button>
-    <!-- <img src="./images/tick.svg" alt="tick image button" class="tick-btn">
-    <img src="./images/cross.svg" alt="close image button" class="del-btn"> -->
+    <button class="del-btn" name="cross" data-value="${virtualDiv.dataset.index}">&cross;</button>
+    <button class="tick-btn" name="tick" data-value="${virtualDiv.dataset.index}">&check;</button>
+    <!-- <img src="./images/cross.svg" alt="close image button" class="del-btn">
+    <img src="./images/tick.svg" alt="tick image button" class="tick-btn"> -->
   </div>`;
-  console.log('this the data-index', virtualDiv.dataset.index);
-  return virtualDiv; // search on how to use appendChild();
+  return virtualDiv;
 };
 
 // dragging and droping
@@ -56,4 +53,83 @@ const getDragPosition = (container, y) => {
   });
 }; */
 
-export default updateTask;
+const addEventHearers = () => {
+  const todosDivs = document.querySelectorAll('.todos'); // dragging and droping
+
+  todosDivs.forEach((draggable) => {
+    draggable.addEventListener('dragstart', () => {
+      draggable.classList.add('dragging');
+    });
+
+    draggable.addEventListener('dragend', () => {
+      draggable.classList.remove('dragging');
+    });
+  });
+
+  // FOR THE DELETE BUTTON
+
+  const delBtn = document.querySelectorAll('.del-btn');
+  const topicDivs = document.querySelectorAll('.topic');
+
+  delBtn.forEach((deleteBtn) => {
+    deleteBtn.addEventListener('click', () => {
+      todosDivs.forEach((thatdiv) => {
+        if (thatdiv.dataset.index === deleteBtn.dataset.value) {
+          topicDivs.forEach((topic) => {
+            if (topic.dataset.counter === deleteBtn.dataset.value) {
+              topic.classList.toggle('deleted');
+            }
+            setTimeout(() => {
+              thatdiv.remove();
+            }, 500);
+          });
+        }
+      });
+    });
+  });
+
+  // ENDING DELETE BUTTON STARTING FOR TICK BUTTON
+
+  const doneBtn = document.querySelectorAll('.tick-btn');
+
+  doneBtn.forEach((tick) => {
+    tick.addEventListener('click', () => {
+      topicDivs.forEach((topic) => {
+        if (topic.dataset.counter === tick.dataset.value) {
+          topic.classList.toggle('done');
+          console.log(`
+            color works now and:
+            topic counter = ${topic.dataset.counter} and tick value = ${tick.dataset.value}
+            `);
+          // topic.remove();
+        } else {
+          console.log(`topic counter = ${topic.dataset.counter} and tick value = ${tick.dataset.value}`);
+        }
+      });
+    });
+  });
+
+  // NOW ENDING BOTH DELETE AND TICK BUTTON
+
+  displayDiv.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    // const afterElement = getDragPosition(displayDiv, e.clientY);
+    const draggable = document.querySelector('.dragging'); // since only only one element will
+    //  have this dragging class, see line 76
+    displayDiv.appendChild(draggable);
+  });
+
+  /*  const getDragPosition = (container, y) => {
+    draggableElements = [...container.querySelectorAll('draggable:not(dragging)')];
+
+    draggableElements.reduce((closest, child) => {
+
+    });
+  }; */
+};
+
+export default {
+  updateTask,
+  addEventHearers,
+  displayDiv,
+};
